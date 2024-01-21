bb06ece4b4533e5d693025ef39d7cac71da6e868
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2024-01-21 12:33:24 -0300

    feat: clicking in link, the sidebar closes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,16 +3,26 @@ const ulNavBarEl = document.querySelector('.navbar__list');
 const navBarJSlogo = document.querySelector('#navbar-JSlogo');
 const navBarHeading = document.querySelector('.navbar__header');
 const navBar = document.querySelector('.navbar');
+const links = document.querySelectorAll('#links')
 
 inputEl.addEventListener("click", () => {
+
   if(inputEl.checked) {
     navBar.classList.add('active');
     ulNavBarEl.classList.add('active');
     navBarHeading.classList.add('active');
-
   } else {
     navBar.classList.remove('active');
     ulNavBarEl.classList.remove('active');
     navBarHeading.classList.remove('active');
   }
 })
+
+links.forEach(links => {
+  links.addEventListener("click", (e) => {
+    navBar.classList.remove('active');
+    ulNavBarEl.classList.remove('active');
+    navBarHeading.classList.remove('active');
+  })
+})
+
