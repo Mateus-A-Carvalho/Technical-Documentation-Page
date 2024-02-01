180249acc28a6fb1cdf7a1bc51df724de4a8da0b
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2024-02-01 12:56:57 -0300

    feat: finally fixed the responsive menu problem. I had a trouble that when I click in links of navbar in mobile viewport, the menu didn't closes. To solve this I made another forEach loop in links to remove 'mobile-active' classes.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,17 +8,19 @@ const links = document.querySelectorAll('#links')
 const mediaQueryMobile = window.matchMedia('(max-width: 425px)');
 const mediaQueryDesktop = window.matchMedia('(min-width: 1023px)');
 
+
 // Triggering open/close menu;
-inputEl.addEventListener("click", () => {
+inputEl.addEventListener("click", (e) => {
   if(inputEl.checked) {
-    navBar.classList.add('active');
-    ulNavBarEl.classList.add('active');
-    navBarHeading.classList.add('active');
+    navBar.classList.toggle('active');
+    ulNavBarEl.classList.toggle('active');
+    navBarHeading.classList.toggle('active');
   } else {
-    navBar.classList.remove('active');
-    ulNavBarEl.classList.remove('active');
-    navBarHeading.classList.remove('active');
+    navBar.classList.toggle('active');
+    ulNavBarEl.classList.toggle('active');
+    navBarHeading.classList.toggle('active');
   }
+
 })
 
 links.forEach(links => {
@@ -30,8 +32,18 @@ links.forEach(links => {
   })
 })
 
-mobileFeatureMenu(mediaQueryMobile);
+links.forEach(links => {
+  links.addEventListener("click", (e) => {
+    navBar.classList.remove('mobile-active');
+    ulNavBarEl.classList.remove('mobile-active');
+    navBarHeading.classList.remove('mobile-active');
+    inputEl.checked = false;
+  })
+})
 
+mobileFeatureMenu(mediaQueryMobile);
+// Problem related: navbar doesn't close when links are clicked;(solved)
+// Problem related: class 'active' continues being added when click are triggred;
 function mobileFeatureMenu(media) {
   inputEl.addEventListener("click", () => {
     if(media.matches && inputEl.checked){
